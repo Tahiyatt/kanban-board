723298d0af7088550dde12e5cc2451fee475f01f
Tahiyat <tahiyatt05@gmail.com>
2025-08-06 16:58:33 -0400

    Milestone React Ready

--- kanban-board/src/components/Counter.tsx
+++ kanban-board/src/components/Counter.tsx
@@ -1,14 +1,23 @@
+'use client'; 
 import { useState } from "react";
 
 type CounterProps = {
   initial: number;
 };
+
 function Counter({ initial }: CounterProps) {
   const [count, setCount] = useState(initial);
 
+  const handleClick = () => {
+    console.log("Button clicked!");  // Debug log
+    setCount(count + 1);  // Increment count
+  };
+
   return (
-    <div className="border-4">
-      <button onClick={() => setCount(count + 1)}> {count}</button>
+    <div className="border-4 testing">
+      <button onClick={handleClick} className="bg-blue-500 text-white p-4 w-50 rounded">
+        {count}
+      </button>
     </div>
   );
 }
