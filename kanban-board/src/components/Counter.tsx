dc47e9709ff9b563275f31234b3c9a64e6010f71
Tahiyat <tahiyatt05@gmail.com>
2025-08-06 12:10:56 -0400

    small change

--- kanban-board/src/components/Counter.tsx
+++ kanban-board/src/components/Counter.tsx
@@ -3,6 +3,14 @@ import {useState} from 'react'
 type CounterProps = {
     inital:number 
 }
-function Counter({inital}:{CounterProps}){
+function Counter({inital}:CounterProps){
+    const [count, setCount] = useState(inital);
 
+    return (
+        <div className="border-4">
+            
+        </div>
+    )
 }
+
+export default Counter;
