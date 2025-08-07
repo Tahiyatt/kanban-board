a83f62f50c825397ad648455e0147536277579dd
Tahiyat <tahiyatt05@gmail.com>
2025-08-07 01:04:19 -0400

    NEXT-READY

--- kanban-board/src/app/about/page.tsx
+++ kanban-board/src/app/about/page.tsx
@@ -1,36 +1,11 @@
-import Image from "next/image";
-import Counter from "../../components/Counter";
+import {Button} from "../../components/ui/button"
 
-
-export default function Home() {
+export default function AboutPage() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-      </main>
-      <div>
-        <h1>Counter Component </h1>
-        <Counter initial={0} />
-      </div>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">About Page</h1>
+      <p>This is a simple static page using the App Router.</p>
+      <Button variant="default">Click me</Button>
     </div>
   );
 }
