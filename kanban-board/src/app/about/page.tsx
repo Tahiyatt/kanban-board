61b5871b3eb9108afaea1a1f052350bac9b64db2
Tahiyat <tahiyatt05@gmail.com>
2025-08-07 16:19:46 -0400

    Milestone Next-Ready

--- kanban-board/src/app/about/page.tsx
+++ kanban-board/src/app/about/page.tsx
@@ -1,13 +1,21 @@
 import {Button} from "../../components/ui/button"
 import Counter from "../../components/Counter"
+import  Link  from "next/link";
 
 export default function AboutPage() {
   return (
-    <div className="p-8">
+    <div className="space-y-4 px-10 py-5">
       <h1 className="text-2xl font-bold">About Page</h1>
       <p>This is a simple static page using the App Router.</p>
-      <Button variant="default">Click me</Button>
-      <Counter initial={0}>Count Value: {count}</Counter>
+     
+      <h1>Below is the counter from Step 3 React Refresher</h1>
+      <Counter initial={0}></Counter>
+
+      <Link href="../api/hello">
+      <Button variant="default">Go to hello page</Button>
+      </Link>
+      
+      
     </div>
   );
 }
